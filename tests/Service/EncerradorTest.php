def3b6879e05abf86ace94d89b8495196f0b53bd
Kevin Araújo <kfa_34@hotmail.com>
2021-11-05 18:25:37 -0300

    Create a mock builder and passing pdo instance as argument into class.

--- tests/Service/EncerradorTest.php
+++ tests/Service/EncerradorTest.php
@@ -21,16 +21,27 @@ class EncerradorTest extends TestCase
       new \DateTimeImmutable('10 days ago')
     );
 
-    $leilaoDao = $this->createMock(LeilaoDao::class);
+    //$leilaoDao = $this->createMock(LeilaoDao::class);
+    $leilaoDao = $this->getMockBuilder(LeilaoDao::class)
+      ->setConstructorArgs([new \PDO('sqlite::memory:')])
+      ->getMock();
+
     $leilaoDao->method('recuperarNaoFinalizados')
       ->willReturn([$fiat147, $variant]);
 
-      $leilaoDao->method('recuperarFinalizados')
-      ->willReturn([$fiat147, $variant]);
+    $leilaoDao->method('recuperarFinalizados')
+    ->willReturn([$fiat147, $variant]);
+
+    $leilaoDao->expects($this->exactly(2))
+      ->method('atualiza')
+      ->withConsecutive(
+        [$fiat147],
+        [$variant]
+      );
 
     $encerrador = new Encerrador($leilaoDao);
     $encerrador->encerra();
-    
+
     $leiloes = [$fiat147, $variant];
     self::assertCount(2, $leiloes);
     self::assertTrue($leiloes[0]->estaFinalizado());
